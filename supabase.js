f7697fe8c0fea05ac4402f275e685b71bfbe7931
5twfmg5k9h-jpg <5twfmg5k9h@privaterelay.appleid.com>
2026-08-28 18:24:29 +0000

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://cgcjownenuvybciwnloa.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_anBmEPm9pXURDjsqjdAbPQ_x1KpO957";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
